0fbb4efd6cde78348161f082138a90ab1a11f576
R2D2CST <r2d2cst@gmail.com>
2026-09-23 16:08:46 -0600

    Env & Main entry

---
+++ Job Seek/Main.py
@@ -0,0 +1,19 @@
+"""
+
+Job Seeker
+
+Created by: R2D2CST
+
+Created: 23/sep/2026
+
+"""
+
+# Python Modules.
+
+# Third Party Libraries.
+
+# Self Built Modules.
+
+if __name__ == "__main__":
+    print("Hello")
+    pass
